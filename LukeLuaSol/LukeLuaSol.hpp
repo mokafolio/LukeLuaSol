932a716345f5ae2ac6cdc8f6ac3d776018c7f62a
Matthias Dörfelt <hi@mokafolio.de>
2018-11-29 15:39:00 -0800

    added converted to allow construction of WindowSettings from lua table

--- LukeLuaSol/LukeLuaSol.hpp
+++ LukeLuaSol/LukeLuaSol.hpp
@@ -11,7 +11,9 @@ namespace lukeLuaSol
 STICK_API inline void registerLuke(sol::state_view _lua, const stick::String & _namespace = "");
 STICK_API inline void registerLuke(sol::state_view _lua, sol::table _tbl);
 
-STICK_API inline sol::table ensureNamespaceTable(sol::state_view _lua, sol::table _startTbl, const stick::String & _namespace)
+STICK_API inline sol::table ensureNamespaceTable(sol::state_view _lua,
+                                                 sol::table _startTbl,
+                                                 const stick::String & _namespace)
 {
     using namespace stick;
 
@@ -417,6 +419,8 @@ STICK_API inline void registerLuke(sol::state_view _lua, sol::table _tbl)
                                      &WindowSettings::setColorPrecision,
                                      "setAlphaPrecision",
                                      &WindowSettings::setAlphaPrecision,
+                                     "setStencilPrecision",
+                                     &WindowSettings::setStencilPrecision,
                                      "setDepthPrecision",
                                      &WindowSettings::setDepthPrecision,
                                      "isResizeable",
@@ -449,7 +453,7 @@ STICK_API inline void registerLuke(sol::state_view _lua, sol::table _tbl)
     tbl.new_usertype<Window>(
         "Window",
         sol::call_constructor,
-        sol::factories([](WindowSettings & _settings) {
+        sol::factories([](const WindowSettings & _settings) {
             std::unique_ptr<Window> wnd(new Window);
             auto err = wnd->open(_settings);
             if (err)
@@ -600,5 +604,81 @@ STICK_API inline void registerLuke(sol::state_view _lua, sol::table _tbl)
 }
 
 } // namespace lukeLuaSol
+
+/* helpers to allow WindowSettings to be created from a table */
+namespace sol
+{
+namespace stack
+{
+
+template <>
+struct checker<luke::WindowSettings>
+{
+    template <typename Handler>
+    static bool check(lua_State * L, int index, Handler && handler, record & tracking)
+    {
+        return sol::stack::check<sol::table>(L, index, handler, tracking);
+    }
+};
+
+template <>
+struct getter<luke::WindowSettings>
+{
+    static luke::WindowSettings get(lua_State * L, int index, record & tracking)
+    {
+        luke::WindowSettings ret;
+
+        sol::table tbl = sol::stack::get<sol::table>(L, lua_absindex(L, index), tracking);
+
+        sol::optional<bool> oResizeable = tbl["resizeable"];
+        if (oResizeable)
+            ret.setResizeable(oResizeable.value());
+
+        sol::optional<bool> oDecorated = tbl["decorated"];
+        if (oDecorated)
+            ret.setDecorated(oDecorated.value());
+
+        sol::optional<const char *> oTitle = tbl["title"];
+        if (oTitle)
+            ret.setTitle(oTitle.value());
+
+        sol::optional<sol::table> oPos = tbl["position"];
+        if (oPos)
+            ret.setPosition(oPos.value()["x"], oPos.value()["y"]);
+
+        sol::optional<sol::table> oSize = tbl["size"];
+        if (oSize)
+            ret.setSize(oSize.value()["width"], oSize.value()["height"]);
+
+        sol::optional<luke::Display &> oDisplay = tbl["display"];
+        if (oDisplay)
+            ret.setDisplay(oDisplay.value());
+
+        sol::optional<stick::UInt32> oSampleCount = tbl["sampleCount"];
+        if (oSampleCount)
+            ret.setSampleCount(oSampleCount.value());
+
+        sol::optional<stick::UInt32> oColPrecision = tbl["colorPrecision"];
+        if (oColPrecision)
+            ret.setColorPrecision(oColPrecision.value());
+
+        sol::optional<stick::UInt32> oAlphaPrecision = tbl["alphaPrecision"];
+        if (oAlphaPrecision)
+            ret.setAlphaPrecision(oAlphaPrecision.value());
+
+        sol::optional<stick::UInt32> oStencilPrecision = tbl["stencilPrecision"];
+        if (oStencilPrecision)
+            ret.setStencilPrecision(oStencilPrecision.value());
+
+        sol::optional<stick::UInt32> oDepthPrecision = tbl["depthPrecision"];
+        if (oDepthPrecision)
+            ret.setDepthPrecision(oDepthPrecision.value());
+
+        return ret;
+    }
+};
+
+} // namespace stack
+} // namespace sol
 
 #endif // LUKELUASOL_LUKELUASOL_HPP
